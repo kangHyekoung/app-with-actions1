91a39b94f2973f1f6d4cd199c94165e570c177cc
kangHyekoung <kanghyekoung3694@gmail.com>
2025-06-02 10:11:02 +0900

    update

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -11,4 +11,9 @@ public class ActionsController {
 		return "app-with-actions version:0.0.1";
 	}
 	
+	@GetMapping(value = "/api/actions1")
+	public String actionsTest1() {
+		return "app-with-actions Test";
+	}
+	
 }
